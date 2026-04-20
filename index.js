25422870ca0de27202ec85648e1dd5230ce9d197
Annabelle Li-Kroeger <alikroe1@jh.edu>
2026-04-20 17:00:12 -0400

    added divs

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const calcButton = document.getElementById("myButton");
 const myDiv = document.getElementById("displayArea");
 
 function calcWakeupTimes() {
-    const sleepTime = new Date()
+    const sleepTime = new Date();
     sleepTime.setMinutes(sleepTime.getMinutes() + 14);
+
     const wakeTime = new Date(sleepTime);
-    const cycleTimes = [];
+    myDiv.innerHTML = "";
+
     for (let i = 1; i <= 6; i++) {
         wakeTime.setMinutes(wakeTime.getMinutes() + 90);
-        cycleTimes.push(wakeTime.toLocaleString("en-US", { timeStyle: "short"}));
+
+        const cycleDiv = document.createElement("div");
+        cycleDiv.setAttribute("id", `cycle-${i}`);
+        cycleDiv.textContent = wakeTime.toLocaleString("en-US", { timeStyle: "short"});
+        myDiv.appendChild(cycleDiv);
     }
-    myDiv.textContent = `Wake up times: ${cycleTimes.join(", ")}`;
 }
 
 calcButton.onclick = calcWakeupTimes;
